e5ca8fb49cdc935bd3fa2761277abcd93b5154c4
Dada Taiwan <pftrew@gmail.com>
2025-06-19 01:23:13 +0800

    Add user profile update functionality and protect routes with middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 5001;
 
 app.use("/api/auth", authRoutes);
