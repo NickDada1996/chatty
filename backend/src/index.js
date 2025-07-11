cc1b515a1b847a19c375a5e35ba3edcad44dcfd5
dada <pftrew@gmail.com>
2025-07-11 16:40:46 -0400

    Update CORS configuration to use the correct production endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://chatty-1wj5b02ro-nicktsais-projects.vercel.app",
+      "https://chatty-ten-rho.vercel.app"
     ],
     credentials: true,
   })
@@ -34,7 +34,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://chatty-1wj5b02ro-nicktsais-projects.vercel.app",
+      "https://chatty-ten-rho.vercel.app"
     ],
     credentials: true,
   },
