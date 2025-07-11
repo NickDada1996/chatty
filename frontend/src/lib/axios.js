16efcc8392689b57b46093a38315e2769d89d224
dada <pftrew@gmail.com>
2025-07-11 16:43:53 -0400

    Fix axios base URL to include the API path

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chatty-9yjz.onrender.com",
+  baseURL: "https://chatty-9yjz.onrender.com/api",
   withCredentials: true,
 });
